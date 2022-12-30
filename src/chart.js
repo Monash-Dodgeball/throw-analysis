87bfaa4bc308798300c858b5b6349bc1b0be891c
Connor Cooper <cooper.connor.17@gmail.com>
2022-12-30 16:02:04 +1100

    Change chart.js to plotly

--- src/chart.js
+++ src/chart.js
@@ -3,26 +3,16 @@ import * as utils from './util.js';
 let myChart;
 
 export function drawChart(poseList, frameCount, keypoint_names) {
-  for (const keypoint_name of keypoint_names) {
-    // TODO multiple joints selected
-    let labels = [...Array(frameCount).keys()]
-    let velocityFunction =  $("#chartVelocityDim").is(":checked") ? utils.velocity3D : utils.velocity2D
-    let yValues = labels.map((i) => velocityFunction(poseList, i, keypoint_name))
+  let velocityFunction = $("#chartVelocityDim").is(":checked") ? utils.velocity3D : utils.velocity2D;
 
-    // TODO update data, shouldn't need to call new Chart
-    if (myChart) myChart.destroy();
-    myChart = new Chart("chart", {
-      type: "line",
-      data: {
-        labels: labels,
-        datasets: [{
-          fill: false,
-          backgroundColor: "rgba(0,0,0,1.0)",
-          borderColor: "rgba(0,0,0,0.1)",
-          data: yValues
-        }]
-      },
-      options: {}
-    });
-  }
+  let labels = [...Array(frameCount).keys()]
+  let data = keypoint_names.map((keypoint_name) => {
+    return {
+      x: labels,
+      y: labels.map((i) => velocityFunction(poseList, i, keypoint_name)),
+      type: 'scatter'
+    }
+  })
+
+  Plotly.newPlot('chart', data);
 }
